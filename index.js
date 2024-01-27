bf5bd2a41b1f2e0fb069ee835263032952414321
shailesh wagh <shaileshwagh300@gmail.com>
2024-01-27 16:04:03 +0530

    file added with some extra functinalit

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
+let postData = async (PostingData) => {
+    try{
+        const response = await fetch('http://localhost:8081/api/codes/use', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                data: PostingData,
+            }),
+        });
+
+        const json = await response.json();
+        console.log(json);
+    }
+    catch (error) {
+        console.error('Error during POST request:', error);
+    }
+};
+
+let getdata=async()=>{
+    try{
+        const response = await fetch('http://localhost:8081/api/codes');
+
+        const json = await response.json();
+        console.log(json);
+    }
+    catch (error) {
+        console.log('Error during GET request:',error);
+    }
+}
+
+
 let handlerefresh=()=>{
     let inputfiled=document.getElementById("inputfield");
     inputfiled.value="";
@@ -6,21 +39,16 @@ let handlerefresh=()=>{
 let handlSubmit=()=>{
     let inputfiled=document.getElementById("inputfield").value;
     let displaytext=document.getElementById("firstInput");
-    console.log(inputfiled);
-    if(inputfiled.length>6){
-        console.log("Wrong");
+    // console.log(inputfiled);
+    if(inputfiled.length>6 || inputfiled.length<=0){
+        // console.log("Enter a valid code");
+        // console.log(inputfiled.length);
+        displaytext.textContent="Enter a valid code";
     }
     else{
-        displaytext.textContent="";
-        let res=fetch("http://localhost:8081/api/codes")
-        .then(res=>res.json())
-        .then(json=>{
-            json.Alldata.map((elem,index)=>{
-                let p1=document.createElement("p");
-                p1.textContent=elem.data;
-                displaytext.append(p1);
-            })
-        })
+        // console.log(inputfiled);
+        postData(inputfiled);
+        getdata();
     }
 
     
